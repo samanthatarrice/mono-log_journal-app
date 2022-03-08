b9df93787b0266887f2d1143d1164d9f3deb8790
Samantha Tarrice <s.tarrice@gmail.com>
2022-03-08 16:39:10 -0300

    Initial navigator setup

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 
 import Home from './components/Home';
+import JournalEntry from './components/JournalEntry';
+import Calendar from './components/Calendar';
+import Settings from './components/Settings';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='+ Journal Entry' component={JournalEntry} />
+        <Tab.Screen name='Calendar' component={Calendar} />
+        <Tab.Screen name='Settings' component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Tab = createBottomTabNavigator();
 
 const styles = StyleSheet.create({
   container: {
--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+
+
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <View>
+      <Text>Home</Text>
+
+    </View>
+    
   )
 }
+
+
 
 export default Home;
