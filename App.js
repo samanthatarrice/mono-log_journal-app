cf343446ea5ac7d0db41d07a97b3e3268c284b30
Samantha Tarrice <s.tarrice@gmail.com>
2022-03-08 22:59:53 -0300

    Begin setup of JournalEntry text and icons

--- App.js
+++ App.js
@@ -1,35 +1,55 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native-web';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+Ionicons
+import { Ionicons } from '@expo/vector-icons';
 
 import Home from './components/Home';
 import JournalEntry from './components/JournalEntry';
 import Calendar from './components/Calendar';
+import Search from './components/Search'
 import Settings from './components/Settings';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='+ Journal Entry' component={JournalEntry} />
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'New Entry') {
+              iconName = 'add-circle';
+            } else if (route.name === 'Calendar') {
+              iconName = 'calendar-outline';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            } else if (route.name === 'Settings') {
+              iconName = 'options-outline'
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'gray',
+          tabBarInactiveTintColor: 'black'
+        })}
+      >
+        <Tab.Screen name='New Entry' component={JournalEntry} />
         <Tab.Screen name='Calendar' component={Calendar} />
+        <Tab.Screen name='Search' component={Search} />
         <Tab.Screen name='Settings' component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const Tab = createBottomTabNavigator();
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16
   },
 });
+
+const Tab = createBottomTabNavigator();
+
+
--- components/JournalEntry.js
+++ components/JournalEntry.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 const JournalEntry = () => {
+  
+  let date = new Date().toDateString();
+
   return (
-    <Text>JournalEntry</Text>
+    <View style={styles.container}>
+      <Text>{date}</Text>
+      <View style={{marginTop: 10, marginRight:'auto', display:'flex', flexDirection:'row'}}>
+        <Icon name='location-outline' type='ionicon' />
+        <Text>Location</Text>
+      </View>
+      <View style={{marginTop: 10, marginRight:'auto', display:'flex', flexDirection:'row'}}>
+        <Icon name='pencil' type='ionicon' />
+        <Text>Title</Text>
+      </View>
+      
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16
+  },
+});
 
 export default JournalEntry;
